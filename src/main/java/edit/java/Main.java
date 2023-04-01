9f97b74588e9078c1d50b984c1d1286da9ac99cf
SplashAni <106507600+SplashAni@users.noreply.github.com>
2023-04-01 07:37:27 +0200

    bye bye :trollface:

--- src/main/java/edit/java/Main.java
+++ src/main/java/edit/java/Main.java
@@ -1,8 +1,8 @@
 package edit.java;
 
-
 public class Main {
-    public static void main(String[] args) {
-        new Setup();
+
+    public static void main(String[] args){
+        System.out.println("this is an entire rewrite bro");
     }
 }
